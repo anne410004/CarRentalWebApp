04c114bbf45b190689c33713474c0e2f2dd33015
Ankita <22051493@kiit.ac.in>
2025-11-09 12:05:34 +0530

    Initial commit

---
+++ src/test/java/com/carrental/CarRentalWebApp/CarRentalWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carrental.CarRentalWebApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarRentalWebAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
